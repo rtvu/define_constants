108c2872dc603f613c8d488d5fdc159f15c9a0a6
ryan <rtvu87@gmail.com>
2021-01-24 10:13:32 -0800

    Added tests

--- test/define_constants_test.exs
+++ test/define_constants_test.exs
@@ -1,4 +1,49 @@
 defmodule DefineConstantsTest do
   use ExUnit.Case
+  import CompileTimeAssertions
   doctest DefineConstants
+
+  defmodule Constants do
+    use DefineConstants
+
+    define(:a, 1)
+    define(:b, "two")
+  end
+
+  test "constant macro provides correct values" do
+    import Constants
+
+    assert constant(:a) === 1
+    assert constant(:b) === "two"
+  end
+
+  test "define macro raises error with non-atom key" do
+    assert_compile_time_raise(ArgumentError, fn ->
+      defmodule Testing do
+        use DefineConstants
+
+        define(1, 1)
+      end
+    end)
+  end
+
+  test "constant raises error with non-atom key" do
+    assert_compile_time_raise(ArgumentError, fn ->
+      defmodule Testing do
+        import Constants
+
+        constant(1)
+      end
+    end)
+  end
+
+  test "constant raises error with undefined key" do
+    assert_compile_time_raise(ArgumentError, fn ->
+      defmodule Testing do
+        import Constants
+
+        constant(:c)
+      end
+    end)
+  end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,22 @@
 ExUnit.start()
+
+# Testing macro exception
+# https://elixirforum.com/t/testing-macro-exception/7105
+defmodule CompileTimeAssertions do
+  defmodule DidNotRaise, do: defstruct(message: nil)
+
+  defmacro assert_compile_time_raise(expected_exception, quoted) do
+    actual_exception =
+      try do
+        Module.eval_quoted(__MODULE__, quoted)
+        %DidNotRaise{}
+      rescue
+        exception ->
+          exception
+      end
+
+    quote do
+      assert unquote(actual_exception.__struct__) === unquote(expected_exception)
+    end
+  end
+end
