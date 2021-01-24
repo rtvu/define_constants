4a7b3575de0653e6d36d68e1f78f754125996b82
ryan <rtvu87@gmail.com>
2021-01-24 10:12:10 -0800

    Updated mix.exs for Hex publishing

--- mix.exs
+++ mix.exs
@@ -1,23 +1,43 @@
 defmodule DefineConstants.MixProject do
   use Mix.Project
 
-  def project do
+  def project() do
     [
       app: :define_constants,
       version: "0.1.0",
       elixir: "~> 1.11",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "DefineConstants",
+      source_url: "https://github.com/rtvu/define_constants",
+      docs: [main: "readme", extras: ["README.md"]]
     ]
   end
 
-  def application do
+  def application() do
     [
       extra_applications: [:logger]
     ]
   end
 
-  defp deps do
-    []
+  defp deps() do
+    [
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Elixir library to provide constants functionality."
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT License"],
+      maintainers: [],
+      links: %{"Github" => "https://github.com/rtvu/define_constants"}
+    ]
   end
 end
